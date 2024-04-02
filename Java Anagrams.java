724818d17f6e97ad47a1f62a02ae343ee942bedc
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-04-02 22:31:39 +0530

    Create Java Anagrams.java

---
+++ Java Anagrams.java
@@ -0,0 +1,72 @@
+Two strings,  and , are called anagrams if they contain all the same characters in the same frequencies. For this challenge, the test is not case-sensitive. For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
+
+Function Description
+
+Complete the isAnagram function in the editor.
+
+isAnagram has the following parameters:
+
+string a: the first string
+string b: the second string
+Returns
+
+boolean: If a and b are case-insensitive anagrams, return true. Otherwise, return false.
+Input Format
+
+The first line contains a string .
+The second line contains a string .
+
+Constraints
+
+Strings  a and b consist of English alphabetic characters.
+The comparison should NOT be case sensitive.
+Sample Input 0
+
+anagram
+margana
+Sample Output 0
+Anagrams
+
+//Solution:
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String s1, String s2) {
+        // Complete the function
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        
+        if(s1.length()==s2.length())
+
+        {
+            int[] a = new int[256];
+            int[] b = new int[256];
+            for (int i = 0; i < s1.length(); i++) {
+                a[(int) s1.charAt(i)] += 1;
+                b[(int) s2.charAt(i)] += 1;
+            }
+            for (int i = 0; i < 256; i++) {
+                if (a[i] != b[i])
+                    return false;
+
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
