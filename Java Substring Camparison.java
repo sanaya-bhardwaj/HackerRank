75abc9d838c7022fd53a61006e87bbe68f037298
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-04-02 21:56:21 +0530

    Create Java Substring Camparison.java

---
+++ Java Substring Camparison.java
@@ -0,0 +1,67 @@
+//We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+
+
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+        for (int i = 0; i <= s.length() - k; i++) {
+            String subStr = s.substring(i, k + i);
+            if (smallest.compareTo(subStr) > 0) {
+                smallest = subStr;
+            } else if (largest.compareTo(subStr) < 0) {
+                largest = subStr;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
